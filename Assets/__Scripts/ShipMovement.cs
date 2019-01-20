446507a8695916b815b2191ca566685bbf524ee2
Argall, Nathan <Nathan.Argall@gmail.com>
2019-01-20 15:44:08 -0600

    initial commit

---
+++ Assets/__Scripts/ShipMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class ShipMovement : MonoBehaviour {
+
+	private float horizMovement;
+	private float verticalMovement;
+	private Vector3 movementWithSpeed;
+	public float speed;
+	
+	void FixedUpdate() {
+		horizMovement = CrossPlatformInputManager.GetAxis("Horizontal");
+		verticalMovement = CrossPlatformInputManager.GetAxis("Vertical");
+		movementWithSpeed = new Vector3(horizMovement, verticalMovement, 0) * speed * Time.deltaTime;
+		transform.position += movementWithSpeed;
+	}
+}
---
+++ Assets/__Scripts/TurretRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class TurretRotation : MonoBehaviour {
+
+	private Vector3 mousePos;
+	private Vector3 turretPos;
+	private Vector3 offset;
+	private float rotateAngle;
+	
+	void FixedUpdate () {
+		mousePos = Camera.main.ScreenToWorldPoint(CrossPlatformInputManager.mousePosition);
+		mousePos.z = -10f;
+		targetPos = transform.position - mousePos;
+		rotateAngle = Mathf.Atan2(targetPos.y, targetPos.x) * Mathf.Rad2Deg;
+	}
+}
